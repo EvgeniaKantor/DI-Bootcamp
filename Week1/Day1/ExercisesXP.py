095b79f3e95bac0932b800ce19e01390a5412569
EvgeniaKantor <evgenia.v.kantor@gmail.com>
2024-03-03 13:48:59 +0200

    all Exercises XP

--- Week1/Day1/ExercisesXP.py
+++ Week1/Day1/ExercisesXP.py
@@ -16,9 +16,29 @@ print("False")
 print("---ex4---")
 computer_brand = "Asus"
 print(f"I have a {computer_brand} computer")
-print("---ex4---")
+print("---ex5---")
 name = 'Evgenia'
 age = "39"
 shoe_size = "39"
 info = f'{name} is {age} years and has {shoe_size} shoe-size'
 print(info)
+print("---ex6---")
+a = 5
+b = 3
+if a>b:
+    print("Hello World")
+print("---ex7---")
+num = int(input("Write a number: "))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+print("---ex8---")
+name = str(input("Write a name: "))
+if name == "Evgenia":
+    print("Hello, I miss you!")
+your_height = int(input("Write a height in cm: "))
+if your_height > 145:
+    print("You are welcome!")
+else:
+    print("You are not tall enough")
