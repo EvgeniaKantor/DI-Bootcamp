de0078552ae26f6dfef2e1c53c3111db2f912103
EvgeniaKantor <evgenia.v.kantor@gmail.com>
2024-03-03 13:16:16 +0200

    second push!

--- Week1/Day1/ExercisesXP.py
+++ Week1/Day1/ExercisesXP.py
@@ -1,1 +1,24 @@
-print("Hello World!")
+print("---ex1---")
+print("Hello World!\n"*4)
+print("---ex2---")
+print((99**3)*8)
+print("---ex3---")
+# 5 < 3
+print("False")
+# 3 == 3
+print("True")
+# 3 == "3"
+print("False")
+# "3" > 3
+print("False")
+# "Hello" == "hello"
+print("False")
+print("---ex4---")
+computer_brand = "Asus"
+print(f"I have a {computer_brand} computer")
+print("---ex4---")
+name = 'Evgenia'
+age = "39"
+shoe_size = "39"
+info = f'{name} is {age} years and has {shoe_size} shoe-size'
+print(info)
