a6690608b501cb253bfe5961b822e261462fe2dc
Evgenia Kantor <160479668+EvgeniaKantor@users.noreply.github.com>
2024-06-21 20:07:58 +0300

    Python Basics, Python Data type

--- Week1/Day1/Exercises_XP_Gold.py
+++ Week1/Day1/Exercises_XP_Gold.py
@@ -1,5 +1,30 @@
+'''Exercise 1 : Hello World-I Love Python
+Instructions
+Print the following output in one line of code:
+Hello world
+Hello world
+Hello world
+Hello world
+I love python
+I love python
+I love python
+I love python
+'''
+####################################################################
 #exercise1
 print("Hello World!\n"*4,"I love python\n"*4)
+####################################################################
+
+'''Exercise 2 : What Is The Season ?
+Instructions
+Ask the user to input a month (1 to 12).
+Display the season of the month received :
+Spring runs from March (3) to May (5)
+Summer runs from June (6) to August (8)
+Autumn runs from September (9) to November (11)
+Winter runs from December (12) to February (2)
+'''
+##################################################################
 #exercise2
 month_names = {
     1: "January",
@@ -33,5 +58,4 @@ if month_number in month_names:
             break
 else:
     print("Invalid month number. Please enter a number between 1 and 12.")
-
-
+#####################################################################
